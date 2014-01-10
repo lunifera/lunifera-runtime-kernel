6641613d30a4979a06882e27ea8bf6df858f1909
Cristiano Gaviao <cvgaviao@gmail.com>
2014-01-10 16:09:26 -0300

    IN PROGRESS - issue LS-28: Kernel library must provide an abstract basic component for gogo command providers https://lunifera.atlassian.net/browse/LS-28

---
+++ org.lunifera.runtime.kernel.library/src/main/java/org/lunifera/runtime/kernel/spi/components/AbstractComponentCommand.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.lunifera.runtime.kernel.spi.components;
+
+import org.lunifera.runtime.kernel.api.components.AbstractComponentCompendium;
+import org.lunifera.runtime.kernel.spi.services.CommandService;
+import org.osgi.service.component.ComponentContext;
+
+/**
+ * @since 0.0.1
+ * @author Cristiano Gavião
+ * 
+ */
+public class AbstractComponentCommand extends AbstractComponentCompendium
+        implements CommandService {
+
+    /**
+     * DS needs a default constructor.
+     */
+    public AbstractComponentCommand() {
+    }
+
+    /**
+     * Constructor created just to help unit testing. It is not used by OSGi.
+     * 
+     * @param componentContext
+     */
+    public AbstractComponentCommand(ComponentContext componentContext) {
+        super(componentContext);
+    }
+
+}
